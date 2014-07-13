9b3a6b692052ce83d6e4afdf5733264455b702c6
brandoshmando <brancraft@gmail.com>
2014-07-13 14:16:49 -0400

    Added form for new picture. Need to put it into a partial later. World Cup final now

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -3,12 +3,22 @@ class PicturesController < ApplicationController
     @pictures = Picture.all
   end
 
+  def new
+    @picture = Picture.new
+  end
+
   def create
-    @picture = Picture.create(pictures_params)
+    @picture = Picture.new(pictures_params)
+    # @picture.user_id = current_user.id
+    if @picture.save
+      redirect_to picture_path(@picture),notice: "Picture successfully created!"
+    else
+      flash.now[:alert] = "Your picture was not created..."
+    end
   end
 private
 
   def pictures_params
-    params.require(:picture).permit(:title, :caption, :author, :user_id)
+    params.require(:picture).permit(:title, :caption, :author)
   end
 end
